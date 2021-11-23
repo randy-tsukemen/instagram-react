267b4da4c65eafb2a6e7342e885f60705edeb752
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-11-23 21:18:19 +0900

    Add tailwindcss forms and search bar

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Image from "next/image";
-import tw from "tailwind-styled-components";
+import {
+  searchIcon,
+  PlusIcon,
+  UserIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MeunIcon,
+  SearchIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
   return (
     <div className="flex justify-between max-w-6xl">
       {/* left */}
       <div
-        className="relative hidden lg:inline-grid h-24 w-24
+        className="relative hidden lg:inline-grid w-24
         cursor-pointer"
       >
         <Image
@@ -28,18 +36,25 @@ const Header = () => {
         />
       </div>
 
-      <h1>Hello world</h1>
-
       {/* Middle */}
-
+      <div className="relative mt-1 p-3 flex items-center">
+        <div
+          className="absolute inset-y-0 pl-3 flex 
+        items-center pointer-events-none"
+        >
+          <SearchIcon className="h-5 w-5 text-gray-500" />
+        </div>
+        <input
+          className="bg-gray-100 w-full pl-10 outline-none
+            border-gray-300 rounded-md focus:ring-black
+            focus:border-black"
+          type="text"
+          placeholder="Search"
+        />
+      </div>
       {/* Right */}
     </div>
   );
 };
 
 export default Header;
-
-const Logo = tw.img`
-  h-24
-  w-24
-`;
