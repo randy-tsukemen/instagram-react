f0d5171781d6263d61fab9759b5f8a24c90bedde
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-11-28 01:55:17 +0900

    style post

--- components/Post.js
+++ components/Post.js
@@ -1,15 +1,30 @@
 import React from "react";
+import {
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  EmojiHappyIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 
 const Post = ({ id, username, userImg, img, caption }) => {
   return (
-    <div>
+    <div className="bg-white my-7 border rounded-sm">
       {/* Header */}
-      <div>
-        <img src={userImg} alt="" />
-        <p>{username}</p>
+      <div className="flex items-center p-5">
+        <img
+          src={userImg}
+          alt=""
+          className="rounded-full h-12 w-12 object-contain border p-1 mr-3 "
+        />
+        <p className="flex-1 font-bold">{username}</p>
+        <DotsHorizontalIcon className="cursor-pointer h-5" />
       </div>
 
       {/* img */}
+      <img src={img} alt="" className="object-cover w-full" />
 
       {/* Button */}
 
