d23c01a39d8eb3b65ef76ec9d0a781f2b6d25f42
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-11-30 20:36:45 +0900

    Add mini profile sample data

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import faker from "faker";
+import { MiniProfiles } from "../data/MiniProfiles";
 
 const Suggestions = () => {
-  return <div></div>;
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const suggestions = MiniProfiles.map((profile) => ({
+      ...profile,
+    }));
+    setSuggestions(suggestions);
+  }, []);
+
+  return (
+    <div className="mt-4 ml-10">
+      <div className="flex justify-between text-sm mb-5">
+        <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
+        <button className="text-gray-600 font-semibold">See all</button>
+      </div>
+
+      {suggestions.map((profile) => (
+        <div
+          className="flex items-center justify-between 
+        mt-3"
+          key={profile.service}
+        >
+          <img
+            src={profile.imgUrl}
+            alt="profile"
+            className="w-10 h-10 rounded-full mr-4"
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Suggestions;
