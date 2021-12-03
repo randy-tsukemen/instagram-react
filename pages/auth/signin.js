3907b658ce85f2d4d7e08f27adfdd72f7ff24991
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-12-03 22:06:12 +0900

    customize style of login icon

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,15 +1,35 @@
 import { getProviders, signIn as SighIntoProvider } from "next-auth/react";
+import Header from "../../components/Header";
 
 const signIn = ({ providers }) => {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => SighIntoProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <Header />
+      <div
+        className="flex flex-col items-center justify-center min-h-screen
+      py-2 -mt-20 px-14 text-center"
+      >
+        <img
+          className="w-80"
+          src="https://pbs.twimg.com/media/FFmSlJXUYAcdG_k?format=jpg"
+          alt="logo"
+        />
+        <p className="font-xs italic">Sign in with your social account</p>
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="p-3 bg-blue-500 rounded-lg text-white"
+                onClick={() =>
+                  SighIntoProvider(provider.id, { callbackUrl: "/" })
+                }
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 };
