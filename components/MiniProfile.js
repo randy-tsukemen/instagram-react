5b6d1599cb6d84002a44d32bbac6da0025d34707
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-12-04 11:09:49 +0900

    customize auth callbacks

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,6 +1,10 @@
+import { useSession } from "next-auth/react";
 import React from "react";
 
 const MiniProfile = () => {
+  const { data: session } = useSession();
+
+  console.log(session);
   return (
     <div
       className="flex items-center justify-between
