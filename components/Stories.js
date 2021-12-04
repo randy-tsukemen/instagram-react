ce6605367012148f320e622d8132a46bedab56f9
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-12-04 21:57:35 +0900

    Add login user icon to Stories

--- components/Stories.js
+++ components/Stories.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Story from "./Story";
 import { Profiles } from "../data/Profiles";
+import { useSession } from "next-auth/react";
 
 const Stories = () => {
   const [suggestions, setSuggestions] = useState([]);
+  const { data: session } = useSession();
 
   useEffect(() => {
     const suggestions = Profiles.map((profile) => ({
@@ -19,6 +21,9 @@ const Stories = () => {
     border-gray-200 border overflow-x-scroll 
       rounded-sm scrollbar-thin scrollbar-thumb-black"
     >
+      {session && (
+        <Story img={session.user.image} username={session.user.username} />
+      )}
       {suggestions.map((profile) => (
         <Story
           key={profile.service}
