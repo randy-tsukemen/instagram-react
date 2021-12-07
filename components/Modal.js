f1121e82754476936be0de4f9951cbf9a655b5a0
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-12-08 01:25:44 +0900

    Add Upload Post button

--- components/Modal.js
+++ components/Modal.js
@@ -7,7 +7,7 @@ const Modal = () => {
   const [open, setOpen] = useRecoilState(modalState);
 
   return (
-    <Transition.Root show={open} as={Fragment}>
+    <Transition show={open} as={Fragment}>
       <Dialog
         as="div"
         className="fixed z-10 inset-0 overflow-y-auto"
@@ -49,12 +49,24 @@ const Modal = () => {
                 overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full
                 sm:p-6"
             >
-              <h1>Hello</h1>
+              <div>
+                <div className="mt-5 sm:mt-6">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center w-full rounded-md border border-transparent
+                    shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700
+                    focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm
+                    disabled:bg-gray-300 disabled:cursor-not-allowed disabled:hover:bg-gray-300"
+                  >
+                    Upload Post
+                  </button>
+                </div>
+              </div>
             </div>
           </Transition.Child>
         </div>
       </Dialog>
-    </Transition.Root>
+    </Transition>
   );
 };
 
