381718a7db0cd7af9ebc1ffa2c85a20dd831272b
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-12-13 01:17:07 +0900

    load posts data from firebase

--- components/Posts.js
+++ components/Posts.js
@@ -1,4 +1,6 @@
-import React from "react";
+import { collection, onSnapshot, orderBy, query } from "@firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
 import Post from "./Post";
 
 const DUMMY_POSTS = [
@@ -29,6 +31,21 @@ const DUMMY_POSTS = [
 ];
 
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+
+    [db]
+  );
+  console.log(posts);
+
   return (
     <div>
       {DUMMY_POSTS.map((post) => (
